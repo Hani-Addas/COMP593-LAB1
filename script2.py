c782b0c1785ef09b35fbaceb01e7f776894e7e6e
Hani-Addas <133828860+Hani-Addas@users.noreply.github.com>
2023-05-17 15:41:43 -0400

    Revert "Modifed both scripts files"
    This reverts commit 2f24d3f44089c8d0dc36184dee655b1e13db28ef.

--- script2.py
+++ script2.py
@@ -1,3 +1,2 @@
 # a script that prints lines form Zen of Python part2
-# another comment part2 
 print('Beautiful is better than ugly.Explicit is better than implicit.Simple is better than complex.Complex is better than complicated.Flat is better than nested.Sparse is better than dense.')
